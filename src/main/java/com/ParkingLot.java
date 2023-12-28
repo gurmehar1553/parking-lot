463bd2bb506a3e122e6f961791beaa1edb2eb497
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-28 20:42:35 +0530

    Added code for unparking car

--- src/main/java/com/ParkingLot.java
+++ src/main/java/com/ParkingLot.java
@@ -9,11 +9,17 @@ public class ParkingLot {
         this.capacity = 100;
         space = new ArrayList<>(capacity);
     }
-    public boolean park(){
+    public boolean park(Car car){
         if (space.size() < 100){
+            space.add(car);
             return true;
         }
         return false;
+    }
+
+    public boolean unpark(Car car) {
+        space.remove(car);
+        return true;
     }
 }
 
--- src/test/java/org/ParkingLotTest.java
+++ src/test/java/org/ParkingLotTest.java
@@ -3,12 +3,23 @@ package org;
 import com.Car;
 import com.ParkingLot;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+    ParkingLot parkingLot;
+    @Before
+    public void setUp(){
+        parkingLot = new ParkingLot();
+    }
     @Test
     public void givenACar_ShouldBeAbleToPark(){
-        ParkingLot parkingLot = new ParkingLot();
-        Assert.assertTrue(parkingLot.park());
+        Car car = new Car();
+        Assert.assertTrue(parkingLot.park(car));
+    }
+    @Test
+    public void givenACar_ShouldBeAbleToUnpark(){
+        Car car = new Car();
+        Assert.assertTrue(parkingLot.unpark(car));
     }
 }
